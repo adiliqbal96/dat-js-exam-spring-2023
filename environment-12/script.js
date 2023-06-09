593988851b76f8002583f6fd504430895dd00a37
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-06-09 15:32:55 +0200

    Env12.2
    1. Indlæs postnummer JSON dataen og fetch postnumre fra postnumre.json
    2. Lav en funktion, der kontrollerer om indtastet postnummer findes i postnrData-listen. Hvis det findes, skal byens navn automatisk blive sat i by inputfeltet. Hvis det ikke findes, skal der vises en fejlmeddelelse.
    3. Tilføj validering til e-mail feltet, for at sikre at det er en gyldig e-mail adresse der bliver indtastet.

--- environment-12/script.js
+++ environment-12/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+let postnrData;
+
+fetch("./postnumre.json")
+  .then((response) => response.json())
+  .then((data) => (postnrData = data))
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+document.getElementById("postnr").addEventListener("input", function (e) {
+  let input = e.target.value;
+
+  for (let data of postnrData) {
+    if (data.postnr === input) {
+      document.getElementById("by").value = data.by;
+      break;
+    } else {
+      document.getElementById("by").value = "";
+    }
+  }
+});
+
+document.getElementById("email").addEventListener("input", function (e) {
+  let input = e.target.value;
+
+  if (!input.includes("@")) {
+    alert("Please enter a valid email");
+  }
+});
