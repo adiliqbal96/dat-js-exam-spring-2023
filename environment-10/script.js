3ddcb8938afb479cd76850a9a3971a0943646a0f
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-06-09 21:49:03 +0200

    Env10.1
    Create a function named loadPosts that loads the JSON data and stores it in a global variable named posts.
    Create a function named displayPosts that displays the posts on the web page. Each post should display with its caption, likes, and image. Only posts that are marked as published should be shown.
    Create a function that filters the posts to only show those with likes greater than 5.

--- environment-10/script.js
+++ environment-10/script.js
@@ -1,1 +1,69 @@
-"use strict";
+let posts = [];
+
+async function loadPosts() {
+  let response = await fetch("posts.json");
+  let data = await response.json();
+  posts = data;
+}
+
+function displayPosts() {
+  let postContainer = document.getElementById("posts-list");
+  postContainer.innerHTML = "";
+
+  let filteredPosts = posts.filter((post) => post.published && post.likes > 5);
+
+  filteredPosts.forEach((post) => {
+    let postElement = document.createElement("article");
+
+    let imageElement = document.createElement("img");
+    imageElement.src = post.image;
+    imageElement.alt = post.caption;
+
+    let captionElement = document.createElement("h2");
+    captionElement.textContent = post.caption;
+
+    let likesElement = document.createElement("p");
+    likesElement.textContent = `Likes: ${post.likes}`;
+
+    postElement.append(imageElement, captionElement, likesElement);
+    postContainer.appendChild(postElement);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await loadPosts();
+  displayPosts();
+});
+
+// Add event listener to filter form checkbox
+document.getElementById("all").addEventListener("change", function () {
+  if (this.checked) {
+    displayPosts();
+  } else {
+    displayFilteredPosts();
+  }
+});
+
+function displayFilteredPosts() {
+  let postContainer = document.getElementById("posts-list");
+  postContainer.innerHTML = "";
+
+  let filteredPosts = posts.filter((post) => post.published);
+
+  filteredPosts.forEach((post) => {
+    let postElement = document.createElement("article");
+
+    let imageElement = document.createElement("img");
+    imageElement.src = post.image;
+    imageElement.alt = post.caption;
+
+    let captionElement = document.createElement("h2");
+    captionElement.textContent = post.caption;
+
+    let likesElement = document.createElement("p");
+    likesElement.textContent = `Likes: ${post.likes}`;
+
+    postElement.append(imageElement, captionElement, likesElement);
+    postContainer.appendChild(postElement);
+  });
+}
