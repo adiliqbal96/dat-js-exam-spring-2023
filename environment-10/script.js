7b46e014fd87625fd7e9e5428acd6c3984bb482a
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-06-09 22:06:42 +0200

    Env10.3
    1. fetch json
    2. display posts
    3. use checkmark to show all posts also unpublished

--- environment-10/script.js
+++ environment-10/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+let posts = [];
+
+async function loadPosts() {
+  const response = await fetch("posts.json"); // Provide the correct path to your JSON file here
+  posts = await response.json();
+}
+
+function displayPosts() {
+  let postsList = document.getElementById("posts-list");
+  postsList.innerHTML = ""; // Clear the current posts before adding new ones.
+
+  let showAll = document.getElementById("all").checked;
+  for (let post of posts) {
+    // Only display the post if it's published or if the "Show all posts" checkbox is checked
+    if (post.published || showAll) {
+      let postArticle = document.createElement("article");
+      postArticle.innerHTML = `
+        <img src="${post.image}" alt="${post.caption}" />
+        <h2>${post.caption}</h2>
+        <p>Likes: ${post.likes}</p>
+      `;
+      postsList.appendChild(postArticle);
+    }
+  }
+}
+
+window.onload = function () {
+  loadPosts().then(() => {
+    displayPosts();
+    // Update posts display when "Show all posts" checkbox changes
+    document.getElementById("all").addEventListener("change", displayPosts);
+  });
+};
