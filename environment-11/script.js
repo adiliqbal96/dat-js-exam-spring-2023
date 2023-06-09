c04f8e5a4a41ee35b8a1c16c6b898634d2362ccc
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-06-09 21:24:13 +0200

    Env11.1

--- environment-11/script.js
+++ environment-11/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+// Definer en global variabel til at holde vores indlæg
+let posts = [];
+
+// Indlæs JSON-data ved hjælp af fetch API
+fetch("./posts.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Gem data i vores globale variabel
+    posts = data;
+    // Kalder vores funktion til at vise indlæg
+    displayPosts();
+  });
+
+// Funktion til at vise indlæg
+function displayPosts() {
+  // Få reference til vores posts-liste sektion
+  const postsList = document.getElementById("posts-list");
+  // Tøm listen (hvis der tidligere er vist indlæg)
+  postsList.innerHTML = "";
+  // Løb gennem hvert indlæg og tilføj det til vores posts-liste sektion
+  posts.forEach((post) => {
+    // Lav et nyt article-element for hvert indlæg
+    const article = document.createElement("article");
+    // Indstil HTML for article-elementet
+    article.innerHTML = `
+            <img src="${post.image}" alt="${post.caption}" />
+            <h2>${post.caption}</h2>
+            <p>Likes: ${post.likes}</p>
+        `;
+    // Tilføj article-elementet til vores posts-liste sektion
+    postsList.appendChild(article);
+  });
+}
