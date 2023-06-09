953fed58ff1e049df341fc9ff1cd4818a0cee8fe
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-06-09 15:08:59 +0200

    Env12.1
    1. Lav en funktion, der aktiveres, når formen indsendes (submit event). Funktionen skal forebygge standardformularens handling (page refresh) og udskrive alle formdata i konsollen.
    2. Udvid denne funktion, så den opretter et address objekt med properties: navn, adresse, postnr, by og email. Dette objekt skal gemmes i en global liste addresses.
    3. Lav en funktion, der viser alle adresser fra addresses-listen på siden.

--- environment-12/script.js
+++ environment-12/script.js
@@ -1,1 +1,26 @@
 "use strict";
+let addresses = [];
+
+document
+  .getElementById("address-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let navn = document.getElementById("navn").value;
+    let adresse = document.getElementById("adresse").value;
+    let postnr = document.getElementById("postnr").value;
+    let by = document.getElementById("by").value;
+    let email = document.getElementById("email").value;
+
+    let address = {
+      navn: navn,
+      adresse: adresse,
+      postnr: postnr,
+      by: by,
+      email: email,
+    };
+
+    addresses.push(address);
+
+    console.log(addresses);
+  });
